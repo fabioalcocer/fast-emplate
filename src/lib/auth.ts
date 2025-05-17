2c4ce75154a3f91e9a180d3556ba95208fb32a19
fabioalcocer <fabioalcocerdev@gmail.com>
2025-05-17 10:55:55 -0400

    first commit

---
+++ src/lib/auth.ts
@@ -0,0 +1,22 @@
+import { env } from "@/env";
+import { db } from "@/lib/db";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { nextCookies } from "better-auth/next-js";
+
+export const auth = betterAuth({
+	database: drizzleAdapter(db, {
+		provider: "pg",
+	}),
+	socialProviders: {
+		github: {
+			clientId: env.GITHUB_CLIENT_ID,
+			clientSecret: env.GITHUB_CLIENT_SECRET,
+		},
+		google: {
+			clientId: env.GOOGLE_CLIENT_ID,
+			clientSecret: env.GOOGLE_CLIENT_SECRET,
+		},
+	},
+	plugins: [nextCookies()],
+});
